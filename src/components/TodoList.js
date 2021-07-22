f32ab823bc2af9ceb139021fc0d0f8b22954c33f
MuslumYilmaz <muslum@vizyon.ai>
2021-07-22 11:29:33 +0300

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,50 @@
+import react, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deleteTodoAction } from '../actions/postActions';
+
+class TodoList extends Component {
+    deleteTodo(id) {
+        this.props.deleteTodo(id);
+    }
+
+    render() {
+        console.log(this.props);
+        const { list } = this.props;
+        const todoList = list.length ? (
+            list.map(l => {
+                return (
+                    <div className="todo-header">
+                        <div>
+                        <ul>
+                            <li key={l.id}>
+                            <p>{l.title}</p>
+                            <button onClick={() => this.deleteTodo(l.id)}>X</button>
+                            </li>
+                        </ul>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (<h3>Nothing to do yet</h3>)
+        return (
+            <div>
+            <h2>Todo List</h2>
+            {todoList}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        list: state.list
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteTodo: (id) => {dispatch(deleteTodoAction(id))}
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,19 @@
+const initState = {
+    list: [
+        {id: 1, title: 'Go shopping'},
+        {id: 2, title: 'Buy milk'}
+    ]
+}
+
+const rootReducer = (state = initState, action) => {
+    if (action.type === "DELETE_TODO") {
+        let newList = state.list.filter(l => l.id !== action.id);
+        return {
+            ...state,
+            list: newList
+        }
+    } 
+    return state;
+}
+
+export default rootReducer;
